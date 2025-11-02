34e41c9f6e3ac5f683f45eb117675be2c2ba03f9
Peyal Hasan <peyalhasan143@gmail.com>
2025-11-02 23:40:09 +0600

    And, OR, Nulish Operator

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,22 @@
+// Just for memory 
+
+const lang = 'Java';
+
+const result1 = lang && "JavaScript";
+console.log(result1) // JavaScript  
+// If left side is truthy then right side will returned;
+
+const result2 = lang || "JavaScript";
+console.log(result2) // Java
+// If left side is falsy then right side will return else left side will return;
+const result3 = lang ?? "JavaScript";
+console.log(result3) // Java
+// Nullish Coalscing Openration: Left side Nullish (Null or Undefined) hole right side will return. 
+
+
 function Item({ name, isPacked }) {
+
+
 
   let itemContent = '';
   if(isPacked){

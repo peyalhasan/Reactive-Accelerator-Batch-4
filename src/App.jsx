1741abeb7c08bc6e1160ce9bdff8e6f28644c83f
Peyal Hasan <peyalhasan143@gmail.com>
2025-11-02 22:58:17 +0600

    Logical and without ternary

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,37 @@
-const person = {
-  name: 'Gregorio Y. Zara',
-  theme: {
-    backgroundColor: 'black',
-    color: 'pink'
-  }
-};
+function Item({ name, isPacked }) {
 
-export default function TodoList() {
+  let itemContent = '';
+  if(isPacked){
+    itemContent = name + '✅'
+  }
+  else{
+    itemContent = name;
+  }
   return (
-    <div style={person.theme}>
-      <h1>{person.name}'s Todos</h1>
-      <img
-        className="avatar"
-        src="https://i.imgur.com/7vQD0fPs.jpg"
-        alt="Gregorio Y. Zara"
-      />
+    <li className="item">
+      {name} {itemContent}
+    </li>
+  );
+}
+
+export default function App() {
+  return (
+    <section>
+      <h1>Sally Ride's Packing List</h1>
       <ul>
-        <li>Improve the videophone</li>
-        <li>Prepare aeronautics lectures</li>
-        <li>Work on the alcohol-fuelled engine</li>
+        <Item 
+          isPacked={true} 
+          name="Space suit" 
+        />
+        <Item 
+          isPacked={true} 
+          name="Helmet with a golden leaf" 
+        />
+        <Item 
+          isPacked={false} 
+          name="Photo of Tam" 
+        />
       </ul>
-    </div>
+    </section>
   );
 }
